ae455f08ead9fcc2bce2c244de852a80daac257e
Shiao-Li <roberto.shiao@epn.edu.ec>
2022-12-13 11:05:44 -0500

    metodos circulos

--- src/Geometria2D/circulo.java
+++ src/Geometria2D/circulo.java
@@ -8,17 +8,20 @@ public class circulo {
     private Scanner sc = new Scanner(System.in);
     public void capturarDatos(){
         try{
-            System.out.print("Ingrese el valor del lado: ");
-            this.lado = sc.nextFloat();
+            System.out.print("Ingrese el valor del radio: ");
+            this.radio = sc.nextFloat();
         } catch (InputMismatchException error) {
             System.err.printf("%nExcepcion (Ingreso incorrecto de datos): %s%n", error);
         }
     }
 
     public void areaCirculo(){
-
+        double areaCirculo = Math.PI * (Math.pow(radio, 2));
+        System.out.println("El area del cirulo es: " + areaCirculo);
     }
-
-
+    public void perimetroCirculo(){
+        double perimetroCirculo = 2 * Math.PI * radio;
+        System.out.println("El perimetro del cirulo es: " + perimetroCirculo);
+    }
 
 }
--- src/Geometria2D/cuadrado.java
+++ src/Geometria2D/cuadrado.java
@@ -1,4 +1,17 @@
 package Geometria2D;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class cuadrado {
+    private double lado, radio, base, altura;
+    private Scanner sc = new Scanner(System.in);
+    public void capturarDatos(){
+        try{
+            System.out.print("Ingrese el valor del radio: ");
+            this.radio = sc.nextFloat();
+        } catch (InputMismatchException error) {
+            System.err.printf("%nExcepcion (Ingreso incorrecto de datos): %s%n", error);
+        }
+    }
 }
