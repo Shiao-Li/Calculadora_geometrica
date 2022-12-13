0bba53a29b4b7b39baf60f4988399da84a3b93f6
Elvis Guanoluisa <elviserock@gmail.com>
2022-12-13 10:48:29 -0500

    metodos completados clase cubo

--- src/Geometria3D/cubo.java
+++ src/Geometria3D/cubo.java
@@ -1,8 +1,28 @@
 package Geometria3D;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class cubo {
-    private double lado, area, volumen;
+    private Scanner sc = new Scanner(System.in);
+    private double lado, areaSuperficial, volumen;
     public void capturarDatos(){
-        System.out.println("");
+        try{
+            System.out.print("Ingrese el valor del lado: ");
+            this.lado = sc.nextFloat();
+        } catch (InputMismatchException error) {
+            System.err.printf("%nExcepcion (Ingreso incorrecto de datos): %s%n", error);
+        }
     }
+
+    public void calcularAreaSuperficial(){
+        areaSuperficial = 6 * Math.pow(lado,2);
+        System.out.println("El area superficial del cubo es: " + areaSuperficial);
+    }
+
+    public void calcularVolumen(){
+        volumen = Math.pow(lado,3);
+        System.out.println("El volumen del cubo es: " + volumen);
+    }
+
 }
--- src/principal.java
+++ src/principal.java
@@ -1,5 +1,11 @@
+import Geometria3D.*;
 public class principal {
     public static void main(String[] args) {
+        //REALIZAR MENU
+        cubo figuraCubo = new cubo();
+        figuraCubo.capturarDatos();
+        figuraCubo.calcularAreaSuperficial();
+        figuraCubo.calcularVolumen();
 
     }
 }
