79710cb7eb8a1131d6cbe9c0ce90a8742043d5c0
Shiao-Li <roberto.shiao@epn.edu.ec>
2022-12-13 10:51:17 -0500

    pedir datos pakage geometria2d circulo

--- src/Geometria2D/circulo.java
+++ src/Geometria2D/circulo.java
@@ -1,4 +1,19 @@
 package Geometria2D;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class circulo {
+    private double lado, radio, base, altura;
+    private Scanner sc = new Scanner(System.in);
+    public void capturarDatos(){
+        try{
+            System.out.print("Ingrese el valor del lado: ");
+            this.lado = sc.nextFloat();
+        } catch (InputMismatchException error) {
+            System.err.printf("%nExcepcion (Ingreso incorrecto de datos): %s%n", error);
+        }
+    }
+
+
 }
